beb822f210f81560d3176149a09677ed803235e1
roshnirifa <roshnirifa998@gmail.com>
2022-06-09 22:40:11 +0600

    about

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -7,24 +7,20 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Sunil Yadav </span>
-            from <span className="purple"> Lucknow, India.</span>
+            Hi Everyone, I am <span className="purple">Afsana Ifat </span>
+            from <span className="purple">Dhaka, Bangladesh</span>
             <br />
-            Software Engineer who loves to transform ideas into reality using
+            FrontEnd Developer who loves to transform ideas into reality using
             code.
             <br />
             Motivated designer and developer with experience creating
-            custom websites with ReactJs, JavaScript, HTML5, CSS3. Shopify app using ReactJs, Javascript, Polaris, Graphql, nodejs, Koajs
-            and Mobile App using React-native.
+            custom websites with ReactJs, JavaScript, HTML5, CSS3.
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
               <ImPointRight /> Playing Games
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
